091ad63b38ea01787a8daa2a71c379db46365d15
monksedo <chris@tnwebdev.com>
2020-02-05 15:04:00 -0800

    Add delete function to delete button

--- app.js
+++ app.js
@@ -20,6 +20,19 @@ function addItems() {
   li.appendChild(document.createTextNode(inputBox.value));
   ul.appendChild(li);
   inputBox.value = '';
+
+  let delBtn = document.createElement("span");
+  var label = document.createTextNode("\u00D7");
+  delBtn.className = "delete";
+  delBtn.appendChild(label);
+  li.appendChild(delBtn);
+
+  for (i = 0; i < close.length; i++) {
+    del[i].onclick = function () {
+      let div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
 }
 
 // Check the input field for user input data
@@ -39,7 +52,7 @@ inputBox.addEventListener('keypress', function (event) {
   }
 });
 
-// Setup add event listener for add button
+// Setup event listener for add button
 btnAdd.addEventListener('click', function () {
   console.log('Working');
   if (checkInput()) {
@@ -50,9 +63,21 @@ btnAdd.addEventListener('click', function () {
 // Creat X button and append it to each list item
 const liList = document.querySelectorAll('li');
 for (let i = 0; i < liList.length; i++) {
-  let del = document.createElement('span');
+  let delBtn = document.createElement('span');
   let label = document.createTextNode('\u00D7');
-  del.className = 'delete';
-  del.appendChild(label);
-  liList[i].appendChild(del);
+  delBtn.className = 'delete';
+  delBtn.appendChild(label);
+  liList[i].appendChild(delBtn);
 }
+
+// Click of the delele "x" button to hide the todo item
+let del = document.querySelectorAll('.delete');
+console.log(del);
+
+for (i = 0; i < del.length; i++) {
+  del[i].addEventListener('click', function () {
+    let div = this.parentElement;
+    div.style.display = 'none';
+  })
+}
+
