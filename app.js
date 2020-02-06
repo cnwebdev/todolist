91b410666610f26baf7e5063a8882d72a74b3780
monksedo <chris@tnwebdev.com>
2020-02-05 19:57:15 -0800

    refactor the functions call code for DRY

--- app.js
+++ app.js
@@ -7,42 +7,30 @@ let inputBox = document.querySelector('.inputBox');
 let btnAdd = document.querySelector('.btnAdd');
 let liList = document.querySelectorAll('li');
 
-// Check the input field for user input data
+// Setup input field event listener
+inputBox.addEventListener('keyup', addItems);
+
+// Setup event listener for add button
+btnAdd.addEventListener('click', addItems);
+
+// Check inputbox for user input data
 function checkInput() {
-  if (inputBox.value === '') {
-    alert('Please enter todo item\'s name!');
-  } else {
+  if (inputBox.value.length >= 3 && event.keyCode === 13) {
     return true;
   }
 };
 
-// Setup input field event listener
-inputBox.addEventListener('keypress', function (event) {
-  console.log(event.keyCode);
-  if (event.keyCode === 13) {
-    addItems();
-  }
-});
-
-// Setup event listener for add button
-btnAdd.addEventListener('click', function () {
-  console.log('Working');
-  if (checkInput()) {
-    addItems();
-  }
-});
-
 // Create element and add todo item to the list
 function addItems() {
-  let ul = document.querySelector('ul');
-  let li = document.createElement('li');
-  li.appendChild(document.createTextNode(inputBox.value));
-  ul.appendChild(li);
-  inputBox.value = '';
-
-  createDeleteButton();
-  // updateList();
-}
+  if (checkInput()) {
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    li.appendChild(document.createTextNode(inputBox.value));
+    ul.appendChild(li);
+    createDeleteButton();
+    inputBox.value = '';
+  }
+};
 
 // Create X button and append it to each list item
 function createDeleteButton() {
@@ -50,16 +38,15 @@ function createDeleteButton() {
   for (let i = 0; i < liList.length; i++) {
     let del = document.createElement('span');
     let label = document.createTextNode('x');
-    del.className = 'delete';
+    del.className = 'done';
     del.appendChild(label);
     liList[i].appendChild(del);
-
-    let delBtn = document.querySelectorAll('.delete');
-    for (let i = 0; i < delBtn.length; i++) {
-      delBtn[i].onclick = function () {
-        let temp = this.parentElement;
-        temp.style.display = 'none';
-      }
+  }
+  let delBtn = document.querySelectorAll('.done');
+  for (let i = 0; i < delBtn.length; i++) {
+    delBtn[i].onclick = function () {
+      let temp = this.parentElement;
+      temp.style.display = 'none';
     }
   }
 };
